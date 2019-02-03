f76073542240b1d0de8b5383171416087931f743
Sujata Dev <sujatadev97@gmail.com>
2019-02-03 20:32:28 +0530

    code formatted

--- src/webscrapeQuizQuestions.py
+++ src/webscrapeQuizQuestions.py
@@ -8,9 +8,9 @@ import csv
 def declare_link():
     main_url = 'https://www.indiabix.com'
 
-    sports_links_list=[]
-    tech_links_list=[]
-    gk_links_list=[]
+    sports_links_list = []
+    tech_links_list = []
+    gk_links_list = []
 
     tech_quiz_url = main_url + '/computer-science/computer-fundamentals/'
     sports_quiz_url = main_url + '/general-knowledge/sports/'
@@ -21,7 +21,6 @@ def declare_link():
     #get_all_links(main_url, gk_quiz_url, gk_links_list)
 
 
-
 def get_all_links(main_url, quiz_url, link_list):
 
     link_list.append(quiz_url)
@@ -30,14 +29,17 @@ def get_all_links(main_url, quiz_url, link_list):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     find_next_pagination_link = soup.find('div', class_='mx-pager-container')
-    pagination_links = [a.get('href') for a in find_next_pagination_link.find_all('a', href=True)]
+    pagination_links = [
+        a.get('href') for a in find_next_pagination_link.find_all(
+            'a', href=True)]
 
     for plink in pagination_links:
         link_list.append(main_url + plink)
 
-
     find_next_section_link = soup.find('ul', class_='ul-top-left')
-    section_links = [a.get('href') for a in find_next_section_link.find_all('a', href=True)]
+    section_links = [
+        a.get('href') for a in find_next_section_link.find_all(
+            'a', href=True)]
 
     for slink in section_links:
         quiz_url = main_url + slink
@@ -46,23 +48,24 @@ def get_all_links(main_url, quiz_url, link_list):
         page = requests.get(quiz_url)
         soup = BeautifulSoup(page.text, 'html.parser')
 
-
-        find_next_pagination_link = soup.find('div', class_='mx-pager-container')
-        pagination_links = [a.get('href') for a in find_next_pagination_link.find_all('a', href=True)]
+        find_next_pagination_link = soup.find(
+            'div', class_='mx-pager-container')
+        pagination_links = [
+            a.get('href') for a in find_next_pagination_link.find_all(
+                'a', href=True)]
 
         for plink in pagination_links:
             link_list.append(main_url + plink)
 
-    #print(link_list)
+    # print(link_list)
     scrape_web_link(link_list)
-
 
 
 def scrape_web_link(link_list):
 
-    i=0
+    i = 0
     for link in link_list:
-        i=i+1
+        i = i + 1
         print(i)
         print(link)
         page = requests.get(link)
@@ -85,26 +88,22 @@ def extract_question_info(soup):
         description_item = options_answer_item.find(
             'div', class_='bix-ans-description')
 
-        difficulty_level = randint(1,3)
+        difficulty_level = randint(1, 3)
         csv_row.append(difficulty_level)
-
-
 
         question = question_text.get_text()
         csv_row.append(question)
 
-
         for td in options_answer_item.find_all('tr'):
-            option_item = td.find('td', {'class': 'bix-td-option', 'width': '99%'})
+            option_item = td.find(
+                'td', {'class': 'bix-td-option', 'width': '99%'})
             options.append(option_item.get_text())
 
         for i in options:
             csv_row.append(i)
 
-
         correct_answer = answer_option.get_text()
         csv_row.append(correct_answer)
-
 
         description = description_item.get_text()
         if re.search("No answer", description):
@@ -112,11 +111,8 @@ def extract_question_info(soup):
 
         csv_row.append(description)
 
-
-        #print(csv_row)
+        # print(csv_row)
         insert_into_csv(csv_row)
-
-
 
 
 def insert_into_csv(row):
@@ -129,4 +125,3 @@ def insert_into_csv(row):
 
 if __name__ == '__main__':
     declare_link()
-

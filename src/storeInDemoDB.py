5d4b152b29bd1642b3c1d460d31bf97d27e6be0f
Sujata Dev <sujatadev97@gmail.com>
2019-02-24 22:30:57 +0530

    added a DB insertion function

---
+++ src/storeInDemoDB.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sqlite3
+from sqlite3 import Error
+
+
+def store_in_DB(CURRENT_ITEMS):
+    '''
+    print(CURRENT_ITEMS[0])
+    print(CURRENT_ITEMS[1])
+    print(CURRENT_ITEMS[2])
+    print(CURRENT_ITEMS[3])
+    print(CURRENT_ITEMS[4])
+    print(CURRENT_ITEMS[5])
+    print("time taken " + CURRENT_ITEMS[6])
+    print("previous ans" + CURRENT_ITEMS[7])
+    '''
+    print("\n")
